2d0329b1c6b1594da37189bbcfc8bd2dee199c07
Allwin Williams Samuvel <allwinwilliams.info@gmail.com>
2018-10-09 12:22:54 +0530

    Updates

---
+++ precache-manifest.c4f0d2175c457b0cc3cb83d4f3396648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "f72eb109ca3d0eb05900",
+    "url": "/react-todo/static/js/main.f72eb109.chunk.js"
+  },
+  {
+    "revision": "144936d1893c77c15f3b",
+    "url": "/react-todo/static/js/1.144936d1.chunk.js"
+  },
+  {
+    "revision": "f72eb109ca3d0eb05900",
+    "url": "/react-todo/static/css/main.882ecaaa.chunk.css"
+  },
+  {
+    "revision": "27cefbb05355c80ff82b58e08926c7fd",
+    "url": "/react-todo/index.html"
+  }
+];
